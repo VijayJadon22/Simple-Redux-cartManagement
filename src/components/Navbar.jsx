e3a36b155241e6687e6cfc33050eaf2c8dd47f2a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-25 22:47:46 +0530

    Implemnted Navbar and took help of Link from react-router-dom for navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="bg-black text-white py-3 px-12 flex justify-between items-center font-bold">
       <h1 className="text-xl">Logo</h1>
       <ul className="flex items-center gap-8 text-lg font-semibold">
-        <li>Home</li>
-        <li>Cart</li>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/cart"}>Cart</Link>
+        </li>
       </ul>
     </div>
   );
