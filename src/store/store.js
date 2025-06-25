0d68a18ed7eb1e8841d1ce53872379bc62055285
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-25 22:47:16 +0530

    Created redux store using toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../features/products/productSlice.js";
+import cartReducer from "../features/cart/cartSlice.js";
 
 const store = configureStore({
     reducer: {
-        products: productReducer
+        products: productReducer,
+        cart: cartReducer
     }
 })
 
