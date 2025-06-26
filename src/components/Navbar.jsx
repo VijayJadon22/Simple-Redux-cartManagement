568c35ceaeae134ef4d3fde173faace81f0f6458
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-26 23:32:51 +0530

    Changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 const Navbar = () => {
+  
   return (
     <div className="bg-black text-white py-3 px-12 flex justify-between items-center font-bold">
       <h1 className="text-xl">Logo</h1>
+      
       <ul className="flex items-center gap-8 text-lg font-semibold">
         <li>
           <Link to={"/"}>Home</Link>
